c2efbed58d690a0b89f329a3dc060cb3d94102bf
Preva1l <prod.preva1l@gmail.com>
2025-07-14 01:45:01 +1000

    fix: maven CDN warning

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,5 +12,5 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.10")
     implementation("com.gradleup.shadow:shadow-gradle-plugin:8.3.6")
     implementation("org.ajoberstar.grgit:org.ajoberstar.grgit.gradle.plugin:5.3.0")
-    implementation("info.preva1l.trashcan:Trashcan-Tooling:1.0.1")
+    implementation("info.preva1l.trashcan:Trashcan-Tooling:1.0.2")
 }
